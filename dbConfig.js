8d1f02cd050ed1bce1c9a561980ebc60e380163f
PetxCode <brighterdayscodelab@gmail.com>
2024-10-05 11:19:54 +0100

    greatly done

--- dbConfig.js
+++ dbConfig.js
@@ -1,5 +1,7 @@
 const { connect } = require("mongoose");
-const URL = "mongodb://127.0.0.1:27017/dribbbleDB";
+// const URL = "mongodb://127.0.0.1:27017/dribbbleDB";
+const URL =
+  "mongodb+srv://skillscapeofficier:skillscapeofficier@cluster0.0dubq.mongodb.net/builderDB?retryWrites=true&w=majority&appName=Cluster0";
 
 const dbConfig = async () => {
   try {
