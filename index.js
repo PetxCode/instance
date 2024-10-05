c882c51b6ac5ac12dc25060fd488f7c5d45f510f
PetxCode <brighterdayscodelab@gmail.com>
2024-10-05 10:58:42 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 8899;
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  try {
+    res.status(200).json({
+      message: "Hello from Dribble API!",
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error" });
+  }
+});
+
+app.listen(process.env.PORT || PORT, () => {
+  dbConfig();
+});
